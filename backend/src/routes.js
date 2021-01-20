781f12f7975b87bd9c6eaa1d9581353dc1950461
Raphael Nascimento <raphael.rsn.123@gmail.com>
2021-01-20 03:50:38 -0300

    criação de rotas / conexão com sqlite / inicio cruds ongs incidents
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>

---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const { request, response } = require('express');
+const express = require('express');
+const IncidentController = require('./controllers/IncidentController');
+const OngController = require('./controllers/OngController');
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;

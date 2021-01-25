61f8c4b2e99c90c764bb7ac29721a4335390b1a0
Raphael Nascimento <raphael.rsn.123@gmail.com>
2021-01-25 02:03:36 -0300

    add routes in App.js
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
+//JSX (javaScript XML)
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Routes />
+    </div>
   );
 }
 

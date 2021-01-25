a13a29e50584aef53a10749c7765ecb679200dbf
Raphael Nascimento <raphael.rsn.123@gmail.com>
2021-01-24 23:19:40 -0300

    finalizaçao backend dia 2
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const { request, response } = require('express');
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 

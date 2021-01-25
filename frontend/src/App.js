1a41aeb7ea97af83a64e57280c0a046ea4d10cd9
Raphael Nascimento <36966098+sump21@users.noreply.github.com>
2021-01-25 03:17:17 -0300

    Omnistack be the hero day 3 (#2)
    * Ajustes para Inicio do react
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add images
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add page logon
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add page register
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add page profile
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add page new incident
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * ajuste btn voltar
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add css global
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add routes
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * add routes in App.js
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>
    
    * finalizado front-end
    
    Signed-off-by: Raphael Nascimento <raphael.rsn.123@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+//JSX (javaScript XML)
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>Sump</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes />
     </div>
   );
 }
